c9b381cf306aa70990a3dea6fb78a1093d68a4a7
Mmmastermind <9kseniatrud@mail.ru>
2025-03-05 21:24:20 +0300

    Add Comments RecipeModul

--- RecipesModul/RecipesModul.cs
+++ RecipesModul/RecipesModul.cs
@@ -10,21 +10,22 @@ namespace RecipesModul
     {
         private List<Recipe> recipes = new List<Recipe>(); //объявление листа, в который будут добавляться рецепты
 
-
+        //добавление рецепта
         public void AddRecipe(Recipe recipe)
         {
-            try
-            {
-                else
-                {
-                    recipe.Id = recipes.Count > 0 ? recipes.Max(r => r.Id) + 1 : 1;
-                    recipes.Add(recipe);
-                }
-            }
-            catch (ArgumentNullException)
+            if (recipe == null) //проверка на нулевое значение
             {
                 throw new ArgumentNullException(nameof(recipe), "Рецепт не может быть null.");
             }
+            
+            try
+            {
+                //автоматически подставляем id
+                    recipe.Id = recipes.Count > 0 ? recipes.Max(r => r.Id) + 1 : 1;
+                    recipes.Add(recipe);
+                
+            }
+           //непредвиденные ошибки
             catch (Exception ex)
             {
                 
@@ -32,17 +33,18 @@ namespace RecipesModul
             }
         }
 
-
+        //удаление рецепта
         public void DeleteRecipe(int id)
         {
             try
-            {
+            { //проверка на нулевое значение
                 if (id <= 0)
                 {
                     throw new ArgumentException("Недопустимый ID рецепта.", nameof(id));
                     
 
                 }
+                //проверка на наличие рецепта с переданным id
                 int initialCount = recipes.Count;
                 recipes.RemoveAll(r => r.Id == id);
                 if (recipes.Count == initialCount)
@@ -51,7 +53,7 @@ namespace RecipesModul
                    
                 }
             }
-          
+            //непредвиденные ошибки
             catch (Exception ex)
             {
                 
@@ -61,30 +63,33 @@ namespace RecipesModul
             
         }
 
-
+        //обновление рецепта
         public void UpdateRecipe(Recipe updatedRecipe)
         {
             
             try
             {
+                //проверка на нулевое значение
                 if (updatedRecipe == null)
                 {
                    
                     throw new ArgumentNullException(nameof(updatedRecipe), "Обновленный рецепт не может быть null.");
                 }
                
-                var existingRecipe = recipes.FirstOrDefault(r => r.Id == updatedRecipe.Id);
+                var existingRecipe = recipes.FirstOrDefault(r => r.Id == updatedRecipe.Id); //переменная, которая хранит рецепты с переданным id рецепта
+                //проверка на наличие изначального рецепта с переданным id
                 if (existingRecipe == null)
                 {
                     throw new KeyNotFoundException($"Рецепт с ID {updatedRecipe.Id} не найден.");
                 }
+                //изменение рецепта
                 existingRecipe.Name = updatedRecipe.Name;
                 existingRecipe.Ingredients = updatedRecipe.Ingredients;
                 existingRecipe.PreparationTime = updatedRecipe.PreparationTime;
                 existingRecipe.Type = updatedRecipe.Type;
                 existingRecipe.Difficulty = updatedRecipe.Difficulty;
             }
-           
+            //непредвиденные ошибки
             catch (Exception ex)
             {
                
@@ -93,14 +98,15 @@ namespace RecipesModul
             }
         }
 
-
+        //получение рецепта по id
         public Recipe GetRecipeById(int id)
         {
             try
             {
-                var recipe = recipes.FirstOrDefault(r => r.Id == id);               
+                //возвращаем список рецептов
+                var recipe = recipes.FirstOrDefault(r => r.Id == id);    //переменная, которая хранит рецепты с переданным id           
                 return recipe;
-            }
+            }//непредвиденные ошибки
             catch (Exception ex)
             {
                     throw new Exception($"Ошибка при получении рецепта с ID {id}.", ex);
@@ -108,13 +114,14 @@ namespace RecipesModul
             
         }
 
-
+        //получение всех рецептов
         public List<Recipe> GetAllRecipes()
         {
             try
             {
+                //возвращаем список рецептов
                 return recipes;
-            }
+            }//непредвиденные ошибки
             catch (Exception ex)
             {
              
@@ -122,7 +129,7 @@ namespace RecipesModul
             }
         }
 
-
+        //получение рецепто по ингредиентам
         public List<Recipe> SearchRecipesByIngredients(List<string> ingredients)
         {
             try
@@ -131,8 +138,9 @@ namespace RecipesModul
                 {
                     throw new ArgumentException("Список ингредиентов не может быть null или пустым.", nameof(ingredients));
                 }
+                //возвращаем список рецептов
                 return recipes.Where(r => r.Ingredients.Intersect(ingredients).Any()).ToList();
-            }
+            }//непредвиденные ошибки
             catch (Exception ex)
             {
                
@@ -140,13 +148,14 @@ namespace RecipesModul
             }
         }
 
-
+        //получение количества рецептов
         public int GetRecipeCount()
         {
             try
             {
+                //возвращаем количество рецептов
                 return recipes.Count;
-            }
+            }//непредвиденные ошибки
             catch (Exception ex)
             {
             
@@ -154,7 +163,7 @@ namespace RecipesModul
             }
         }
 
-
+        //получение рецептов по типу
         public List<Recipe> GetRecipesByType(string type)
         {
             try
@@ -163,8 +172,9 @@ namespace RecipesModul
                 {
                     throw new ArgumentException("Тип не может быть null или пустым.", nameof(type));
                 }
+                //возвращаем список рецептов
                 return recipes.Where(r => r.Type == type).ToList();
-            }
+            }//непредвиденные ошибки
             catch (Exception ex)
             {
                 
@@ -172,18 +182,19 @@ namespace RecipesModul
             }
         }
 
-
+        //получение рецептов по сложности
         public List<Recipe> GetRecipesByDifficulty(string difficulty)
         {
             try
-            {
+            {//проверка на нулевое значение
                 if (string.IsNullOrEmpty(difficulty))
                 {
                     throw new ArgumentException("Сложность не может быть null или пустой.", nameof(difficulty));
                 }
+                //возвращаем список рецептов
                 return recipes.Where(r => r.Difficulty == difficulty).ToList();
             }
-            
+            //непредвиденные ошибки
             catch (Exception ex)
             {
                
@@ -191,20 +202,15 @@ namespace RecipesModul
             }
         }
 
-
+        //вывод рецептов по времени приготовления
         public List<Recipe> GetRecipesByPreparationTime(int maxTime)
-        {
-            try
+        {// проверка, что время передано правильно
+            if (maxTime <= 0)
             {
-
-                return recipes.Where(r => r.PreparationTime <= maxTime).ToList();
+                throw new Exception("Время приготовления должно быть больше 0");
             }
-            catch (Exception)
-            {
-
-                throw new Exception($"Ошибка при получении рецептов по времени приготовления.");
-            }
-
+            //возвращаем список рецептов
+            return recipes.Where(r => r.PreparationTime <= maxTime).ToList();
         }
     }
 }
