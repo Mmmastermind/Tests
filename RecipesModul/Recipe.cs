5ab526d0885e268d68daea2b2957ca670817ba14
Mmmastermind <9kseniatrud@mail.ru>
2025-03-05 17:24:58 +0300

    Update Recipe.cs

--- RecipesModul/Recipe.cs
+++ RecipesModul/Recipe.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+//класс для создания структуры рецептов
 namespace RecipesModul
 {
     public class Recipe
