2908959734d803c54342d3a4c2165896fbb37e0f
Mmmastermind <9kseniatrud@mail.ru>
2025-03-05 13:17:43 +0300

    Create Recipe.cs

---
+++ RecipesModul/Recipe.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecipesModul
+{
+    public class Recipe
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<string> Ingredients { get; set; }
+        public int PreparationTime { get; set; }
+        public string Type { get; set; }
+        public string Difficulty { get; set; }
+    }
+}
